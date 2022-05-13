09ddfd6f50a07357514e8c008ef242a924bcfb05
nickbalan <nickolae.balan@gmail.com>
2022-05-13 02:01:42 +0200

    Implementing the Delete Expenses button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
+  const {dispatch} = useContext(AppContext)
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: 'DELETE_EXPENSE',
+      payload: props.id,
+    });
+  };
+
   return (
     <li className='list-group-item d-flex justify-content-between align-items-center'>
       {props.name}
@@ -9,7 +18,7 @@ const ExpenseItem = (props) => {
         <span className='badge badge-primary badge-pill mr-3'>
           €{props.cost}
         </span>
-        <TiDelete size='1.5em'></TiDelete>
+        <TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
       </div>
     </li>
   );
